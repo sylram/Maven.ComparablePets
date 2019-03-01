9cc2772701849d3f16a95f32d0434d883d844fcc
sylram <sylram7@gmail.com>
2019-03-01 16:06:33 -0500

    cat and fish tests

--- src/test/java/io/zipcoder/CatTest.java
+++ src/test/java/io/zipcoder/CatTest.java
@@ -1,4 +1,56 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CatTest {
+
+
+
+    @Test
+    public void getNameTest(){
+        //Given
+        String givenName = "Bella";
+        Cat cat = new Cat(null);
+
+
+        //When
+        String expected = cat.getName();
+
+        //Then
+        Assert.assertEquals(givenName,expected);
+    }
+
+
+
+    @Test
+    public void setNameTest(){
+        //Given
+        String nameGiven = "Bella";
+        Cat cat = new Cat(null);
+
+
+        //When
+        cat.setName();
+        String expected = cat.getName();
+
+        //Then
+        Assert.assertEquals(nameGiven,expected);
+    }
+
+    @Test
+    public void catSpeakTest(){
+        //Given
+        String nameGiven = "Bella";
+        Cat cat = new Cat(null);
+        String sound = "meow!";
+
+
+        //When
+        String expected = cat.speak();
+
+        //Then
+        Assert.assertEquals(nameGiven,expected);
+    }
+
 }
--- src/test/java/io/zipcoder/FishTest.java
+++ src/test/java/io/zipcoder/FishTest.java
@@ -1,4 +1,50 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FishTest {
+
+    @Test
+    public void getNameTest(){
+        //Given
+        String givenName = "Victor";
+        Fish fish = new Fish();
+
+        //When
+        String expected = fish.getName();
+
+        //Then
+        Assert.assertEquals(givenName,expected);
+
+
+
+    }
+
+    @Test
+    public void setNameTest(){
+        //Given
+        String givenName = "Victor";
+        Fish fish = new Fish();
+
+        //When
+        String expected = fish.setName();
+
+        //Then
+        Assert.assertEquals(givenName,expected);
+
+    }
+    @Test
+    public void fishSpeakTesh(){
+        //Given
+        String givenName = "Victor";
+        Fish fish = new Fish();
+        String fishSound = "glu"
+
+        //When
+        String expected = fish.speak();
+
+        //Then
+        Assert.assertEquals(givenName,expected);
+    }
 }
