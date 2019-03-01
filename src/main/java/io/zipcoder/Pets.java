ec35304ad0d649430fda9e4db7c39337deb1bde3
Eric Jefferson <ejefferson50@gmail.com>
2019-03-01 16:03:11 -0500

    Added Pets tests

--- src/main/java/io/zipcoder/Pets.java
+++ src/main/java/io/zipcoder/Pets.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
+import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
+
 public class Pets {
+
+
+    public static Dog getNewPetDog(String name) {
+
+        return null;
+    }
+
+    public static Cat getNewPetCat(String name) {
+
+        return null;
+    }
+
+    public static Fish getNewPetFish(String name) {
+
+        return null;
+    }
 }
