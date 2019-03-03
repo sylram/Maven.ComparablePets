a7872eb5d3e5a6005f4d0e9da0c3884e7e955d8f
Eric Jefferson <ejefferson50@gmail.com>
2019-03-02 21:37:15 -0500

    Added PetsComparatorTest

--- src/main/java/io/zipcoder/PetsComparator.java
+++ src/main/java/io/zipcoder/PetsComparator.java
@@ -4,7 +4,5 @@ import java.util.Comparator;
 
 public class PetsComparator implements Comparator<Pets> {
 
-    public int compare(Pets pet1, Pets pet2) {
-        return 0;
-    }
+
 }
--- src/test/java/io/zipcoder/PetsComparatorTest.java
+++ src/test/java/io/zipcoder/PetsComparatorTest.java
@@ -1,7 +1,65 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class PetsComparatorTest {
+public class PetsComparatorTest {
+
+
+    @Test
+    public void comparePetsTest(){
+
+        //Given
+        Pets pet1 = new Pets("Max");
+        Pets pet2 = new Pets("Chloe");
+        int expected = -1;
+
+        //When
+        int actual = PetsComparator.compare(pet1, pet2);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void comparePetsTest2(){
+
+        //Given
+        Pets pet1 = new Pets("Max");
+        Pets pet2 = new Pets("Max")
+        int expected = 0;
+
+        //When
+        int actual = PetsComparator.compare(pet1, pet2);
+
+        //
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+        @Test
+        public void comparePetsTest3(){
+            //Given
+            Pets pet1 = new Pets("Max");
+            Pets pet2 = new Pets("Ned"):
+            int expected = 1;
+
+            //When
+            int actual = PetsComparator.compare(pet1, pet2);
+
+            //Then
+            Assert.assertEquals(expected,actual);
+
+
+
+
+        }
+
+
+
 
 }
